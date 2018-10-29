8635ad7680d066ced1851732ec2b838ffd5dc8dc
Aleksey Smirnov <lirien42@gmail.com>
2018-10-30 01:02:00 +0300

    exercise 1.7 implementation

--- sicp/chapter1/07.rkt
+++ sicp/chapter1/07.rkt
@@ -5,8 +5,8 @@
 (define (square x)
   (* x x))
 
-(define (good-enough? guess x)
-(< (abs (- (square guess) x)) 0.001))
+(define (good-enough? guess new-guess)
+(< (abs (- guess new-guess)) 0.001))
 
 (define (new-if predicate then-clause else-clause)
   (cond (predicate then-clause)
@@ -18,18 +18,13 @@
 (define (average x y)
 (/ (+ x y) 2))
 
-;new-if is not suitable for this case because 
-;new-if is a function that try to calculate it's argument
-;(sqrt-iter (improve guess x) x)
-;and we get the error - endless cycle.
 (define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+  (define new-guess (improve guess x))
+  (if (good-enough? guess new-guess)
     guess
     (sqrt-iter (improve guess x) x)))
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
 
-;(sqrt 1000000000000000) - endless cycle.
-
-(sqrt 9)
+(sqrt 1000000000000000)
